bee515ad1489456da27062cdbab41b3bf052ee85
Bhavik <bhavikpatel773241@gmail.com>
2026-03-09 14:53:49 -0400

    feat: add helper utilities module

---
+++ src/utils/helpers.js
@@ -0,0 +1,4 @@
+export const clamp = (v, min, max) => Math.min(Math.max(v, min), max);
+export const uuid = () => Math.random().toString(36).slice(2);
+export const sleep = ms => new Promise(r => setTimeout(r, ms));
+export const formatBytes = b => b < 1024 ? b+"B" : b < 1048576 ? (b/1024).toFixed(1)+"KB" : (b/1048576).toFixed(1)+"MB";
